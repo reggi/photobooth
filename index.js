d41d04f809a513cd4bfbb152ca107b5cad2f8038
Thomas Reggi <socialtr@gmail.com>
2015-10-29 21:58:49 -0400

    take pic

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+var moment = require('moment')
+
+var child_process = require('child_process')
+var exec = child_process.exec
+
 var Gpio = require('onoff').Gpio
 var button = new Gpio(4, 'in', 'both')
- 
+
+function takePicture () {
+ var timeStamp = moment().format('x')
+ var command = 'fswebcam -r 640x480 --jpeg 85 -D 1 photo-'+timeStamp+'.jpg'
+ return exec(command, function (err) {
+  console.log('ran')
+ }) 
+}
+
 button.watch(function(err, value) {
-  console.log(value, "hello world")
+  if (value === 1) takePicture()
 });
