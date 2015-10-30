824738261c5b5e4591f1547822a24670abbe4494
Thomas Reggi <socialtr@gmail.com>
2015-10-29 21:53:07 -0400

    log value

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var Gpio = require('onoff').Gpio
 var button = new Gpio(4, 'in', 'both')
  
 button.watch(function(err, value) {
-  console.log("hello world")
+  console.log(value, "hello world")
 });
