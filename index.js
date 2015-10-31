ede201fcd0ad62760c81181afb850bb9d84f68d7
Thomas Reggi <socialtr@gmail.com>
2015-10-30 22:34:33 -0400

    imgur

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var imgur = require('imgur-node-api')
+var path = require('path')
 var moment = require('moment')
 
 var child_process = require('child_process')
@@ -6,11 +8,24 @@ var exec = child_process.exec
 var Gpio = require('onoff').Gpio
 var button = new Gpio(4, 'in', 'both')
 
+console.log('running')
+
+function sendToImgur(imagePath) {
+ imgur.setClientID(process.env.IMGUR_KEY);
+ imgur.upload(imagePath, function (err, res) {
+  if (err) throw err
+  console.log(res.data.link)
+ })
+}
+
 function takePicture () {
  var timeStamp = moment().format('x')
- var command = 'fswebcam -r 1600x1200 --jpeg 85 -D 1 photo-'+timeStamp+'.jpg --no-overlay'
+ var photoPath = '../photos/photo-'+timeStamp+'.jpg'
+ var command = 'fswebcam -r 1600x1200 --jpeg 85 -D 1 ' + photoPath + ' --no-overlay'
  return exec(command, function (err) {
+  if (err) throw err
   console.log('ran')
+  return sendToImgur(photoPath)
  }) 
 }
 
